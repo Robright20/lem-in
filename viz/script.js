b1e6194475c7d5d3867d0a7845aef2ecbc6e8539
Robert Bright <robright28@gmail.com>
2020-11-02 09:54:41 +0100

    updates

--- viz/script.js
+++ viz/script.js
@@ -56,16 +56,26 @@ const drawEdge = (edge) => {
 
 	edge.html.className = 'line';
 	edge.html.style.width = edge.length + 'px';
-	edge.html.style.transform = 'rotate(' + edge.angle + 'rad)';
+	edge.html.style.transform = 'rotate(' + (Math.PI - edge.angle) + 'rad)';
 	edge.html.style.left = edge.coords.x + 'px';
 	edge.html.style.top = edge.coords.y + 'px';
 	canvas.append(edge.html);
 }
 let node1 = new Node("bob");
-node1.coords = {x: 50, y: 100};
 let node2 = new Node("laura");
+let node3 = new Node("dany");
+let node4 = new Node("jasmine");
+node1.coords = {x: 50, y: 100};
 node2.coords = {x: 250, y: 250};
+node3.coords = {x: 80, y: 140};
+node4.coords = {x: 100, y: 50};
 node1.linkWith(node2);
+node1.linkWith(node3);
+node1.linkWith(node4);
 drawNode(node1);
 drawNode(node2);
-drawEdge(node1.edges[0]);
+drawNode(node3);
+drawNode(node4);
+//drawEdge(node1.edges[0]);
+//drawEdge(node1.edges[1]);
+drawEdge(node1.edges[2]);
