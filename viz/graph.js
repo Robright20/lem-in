85a412c51ca8b224030ed3fc059994015c2bb4da
Robert Bright <robright28@gmail.com>
2020-11-28 12:40:17 +0100

    refactor graph_js

--- viz/graph.js
+++ viz/graph.js
@@ -1,141 +1,150 @@
 'use strict'
+
+// import Graphic from './graphics.js';
 const log = console.log;
-const nodes = ['start', 'end'];
-const edges = [];
-const layers = [];
-const scale = 20;
-const adjust = {x: 300, y: 250}
-
-nodes.add = function (data, cmd) {
-	if (!/^[#L]/.test(data[0])) {
-		let node = new Node(data[0]);
-
-		node.coords = {
-			x: Number(data[1]) * scale + adjust.x,
-			y: Number(data[2]) * scale + adjust.y
-		};
-		node.id = nodes.length;
-		if (cmd === '##start') {
-			this[0] = node;
-			node.id = 0;
-		} else if (cmd === '##end') {
-			this[1] = node;
-			node.id = 1;
-		} else {
-			nodes.push(node);
-		}
-		return node;
-	}
-}
-
-nodes.find = function(name) {
-	for (const key in this) {
-		if (this[key].name === name) {
-			return this[key];
-		}
-	}
-}
-
-edges.find = function(name) {
-	for (const key in this) {
-		if (this[key].name === name) {
-			return this[key];
-		}
-	}
-}
-
-const newEdge = (from, to) => {
-	if (from && to) {
-		return ({
-			_from: from.id,
-			_to: to.id,
-			get from() {
-				return (nodes[this._from]);
-			},
-			get to() {
-				return (nodes[this._to]);
-			},
-			set from(node) {
-				this._from = node.id;
-			},
-			set to(node) {
-				this._to = node.id;
-			}
-		});
-	}
-};
 
 class Node {
-	constructor(name) {
+	constructor(id, name) {
+		if (typeof id !== 'number' || typeof name !== 'string')
+			throw Error(`from: ${from}, to: ${to}`)
+		this.id = id;
 		this.name = name;
 		this.coords = {x: 0, y: 0};
+		this.neighbrs = [];
 		this.edges = [];
-		this.html = '';
 		this.radius = 5;
 	}
-}
-
-function createGraph(data) {
-	let cols = [];
-	let edge = {};
-	let cmd = '';
-
-	for (let row in data) {
-		if (/^(#){2}/.test(data[row])) {
-			if (data[row] === '##end-farm') {
-				return { nodes: nodes, edges: edges };
-			} else if ((data[row] == '##start' || data[row] == '##end') && cmd === '') {
-				cmd = data[row];
-			}
-		} else if (/^\w+ \d+ \d+$/.test(data[row])) {
-			cols = data[row].split(' ');
-			nodes.add(cols, cmd);
-			cmd = '';
-		} else if (/^\w+-\w+$/.test(data[row]) && !/^[L#]/.test(data[row])) {
-			cols = data[row].split('-');
-			edge = newEdge(nodes.find(cols[0]), nodes.find(cols[1]));
-			if (edge) {
-				edge.id = edges.length;
-				edge.name = data[row];
-				edge.from.edges.push(edges.length);
-				edge.to.edges.push(edges.length);
-				edges.push(edge);
-				cmd = '';
-			}
-		}
+	addLink = (edge) => {
+		let to = edge.from;
+		if (to.id === this.id)
+			to = edge.to;
+		this.neighbrs.push(to.id);
+		this.edges.push(edge.id);
 	}
 }
 
-let max = 0;
-
-function build_layers(nodes) {
-	let cur = 0;
-	let edge = [];
-	let to = {};
-	layers[0] = [nodes[0]];
-	nodes[0].visited = true;
-
-	do {
-		layers[cur].forEach(node => {
-			node.edges.forEach(edgeId => {
-				edge = edges[edgeId];
-				to = node === edge.from ? edge.to : edge.from;
-				if (to.visited === undefined) {
-					to.visited = true;
-					if (layers[cur + 1]) {
-						layers[cur + 1].push(to);
-					} else {
-						layers[cur + 1] = [to];
-					}
-				}
-			})
-		});
-		if (layers[cur].length > layers[max].length) {
-			max = cur
-		}
-		cur += 1;
-	} while (layers[cur]);
-	return (layers);
+class Edge {
+	constructor(from, to) {	
+		if (from && to) {
+			this.from = from;
+			this.to = to;
+		} else
+			throw Error(`from: ${from}, to: ${to}`)
+	}
 }
 
-export { createGraph, nodes, edges, build_layers, layers, max};
+export default class Graph {
+	constructor(data) {
+		let cols, cmd;
+		this._nodes = ['start', 'end'];
+		this._edges = [];
+		this._layers = [];
+		this._layersMax = 0;
+		this._index = {};
+	
+		for (let row in data) {
+			if (/^(#){2}/.test(data[row])) {
+				if (data[row] === '##end-farm') return
+				else if ((data[row] == '##start' || data[row] == '##end') && cmd == null)
+					cmd = data[row];
+			} else if (/^\w+ \d+ \d+$/.test(data[row])) {
+				cols = data[row].split(' ');
+				this.addNode(cols, cmd);
+				cmd = null;
+			} else if (/^\w+-\w+$/.test(data[row]) && !/^[L#]/.test(data[row])) {
+				cols = data[row].split('-');
+				if (this.addEdge(cols, data[row]))
+					cmd = null;
+			}
+		}
+	}
+	addNode = (data, cmd) => {
+		const scale = 20;
+		const adjust = {x: 300, y: 250};
+		let node;
+		
+		if (!/^[#L]/.test(data[0])) {
+			node = new Node(this._nodes.length, data[0]);
+			node.coords = {
+				x: Number(data[1]) * scale + adjust.x,
+				y: Number(data[2]) * scale + adjust.y
+			};
+			if (cmd === '##start') {
+				node.id = 0;
+				this._nodes[0] = node;
+			} else if (cmd === '##end') {
+				node.id = 1;
+				this._nodes[1] = node;
+			} else
+				this._nodes.push(node);
+			this._index[node.name] = node.id;
+			return node;
+		}
+	}
+	addEdge = (data, name) => {
+		try {
+			let edge = new Edge(this.find({nodes: data[0]}), this.find({nodes: data[1]}));
+
+			if (edge) {
+				edge.id = this._edges.length;
+				edge.name = name;
+				edge.from.addLink(edge)
+				edge.to.addLink(edge)
+				this._edges.push(edge);
+				this._index[edge.name] = edge.id;
+				return true;
+			}
+		} catch (err) { log(err) }
+	}
+	find = (options) => {
+		let result;
+
+		if (typeof options !== 'object') return
+		Object.keys(options).forEach(key => {
+			let id;
+
+			if (key === 'nodes') {
+				id = this._index[options['nodes']];
+				if (id != null) result = this._nodes[id];
+			} else if (key === 'edges') {
+				id = this._index[options['edges']];
+				if (id != null) result = this._edges[id];
+			}
+		})
+		return result;
+	}
+	buildLayers = () => {
+		let cur = 0;
+		this.layers[0] = [this.nodes[0]];
+		this.nodes[0].visited = true;
+		
+		do {
+			this.layers[cur].forEach(node => {
+				node.neighbrs.forEach(id => {
+					if (this.nodes[id].visited == null) {
+						this.nodes[id].visited = true;
+						if (this.layers[cur + 1])
+							this.layers[cur + 1].push(this.nodes[id]);
+						else
+							this.layers[cur + 1] = [this.nodes[id]];
+					}
+				})
+			});
+			if (this.layers[cur].length > this.layersMax.length)
+				this._layersMax = cur;
+			cur += 1;
+		} while (this.layers[cur]);
+	}
+	get nodes() {
+		return this._nodes;
+	}
+	get edges() {
+		return this._edges;
+	}
+	get layers() {
+		return this._layers;
+	}
+	get layersMax() {
+		return this._layers[this._layersMax];
+	}
+}
